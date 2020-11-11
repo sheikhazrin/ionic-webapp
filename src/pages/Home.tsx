aeacfb3fc464b2a0739fa7eb7e3ead77ddfced89
Sheik Hazrin Bin Sheik Othman <sheikhazrin@outlook.my>
2020-11-12 06:06:09 +0800

    ionic first try

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,7 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { camera, trash, close } from 'ionicons/icons';
+import { IonFab, IonFabButton, IonIcon, IonGrid, IonRow,
+         IonCol, IonImg, IonActionSheet } from '@ionic/react';
 import React from 'react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
@@ -8,13 +11,13 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>APP TITLE <IonIcon icon={camera} /></IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">LOGO</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
